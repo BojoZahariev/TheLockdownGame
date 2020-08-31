fcd4d273b060dbbac86bc98f39a17d7a352dc818
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-31 11:04:30 +0100

    click to move

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var teamBlue = document.querySelector('.blue');
-var teamRed = document.querySelector('.red');
+var teamBlue = document.querySelectorAll('.frame');
+//var teamRed = document.querySelector('.red');
 var box = document.querySelector('.box');
 
-teamBlue.addEventListener('click', changeTeam);
-teamRed.addEventListener('click', changeTeam);
+Array.from(teamBlue).forEach(element => element.addEventListener('click', changeTeam));
+//teamRed.addEventListener('click', changeTeam);
 
 function changeTeam() {
   var rect = box.getBoundingClientRect();
@@ -11,13 +11,13 @@ function changeTeam() {
   this.appendChild(box);
 
   TweenMax.set(box, { x: 0, y: 0 });
-
+  /*
   if (classes.contains('red')) {
     TweenMax.to(box, 1, { backgroundColor: 'red' });
   } else if (classes.contains('blue')) {
     TweenMax.to(box, 1, { backgroundColor: 'blue' });
   }
-
+*/
   var newRect = box.getBoundingClientRect();
 
   TweenMax.from(box, 1, {
