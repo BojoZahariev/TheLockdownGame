3e2fc4ed6fd5a2a359055bb665675892e8273ab1
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-31 12:44:20 +0100

    move to index

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 var teamBlue = document.querySelectorAll('.frame');
 var box = document.querySelector('.box');
+var dice = document.querySelector('#dice');
 
 Array.from(teamBlue).forEach(element => element.addEventListener('click', changeFrame));
 
-function changeFrame() {
+function changeFrame(position) {
+  console.log(position);
   var rect = box.getBoundingClientRect();
-  this.appendChild(box);
+  Array.from(teamBlue)[Array.from(teamBlue).indexOf(box.parentElement) + position].appendChild(box);
 
   TweenMax.set(box, { x: 0, y: 0 });
 
   var newRect = box.getBoundingClientRect();
 
-  TweenMax.from(box, 1, {
+  //animation speed and ease
+  TweenMax.from(box, 4, {
     x: rect.left - newRect.left,
     y: rect.top - newRect.top,
     ease: Power1.easeInOut
   });
 }
 
+dice.addEventListener('click', () => {
+  //console.log(Array.from(teamBlue).indexOf(box.parentElement));
+  changeFrame(rollDice());
+});
+
 //DICE
 
 function rollDice() {
   return 1 + Math.floor(Math.random() * 6);
 }
-
-console.log(rollDice());
