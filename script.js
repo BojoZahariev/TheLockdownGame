cf68826a641ed089e8ca754ad38e8e7b2b1ebcba
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-31 12:18:58 +0100

    animation changes

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
 var teamBlue = document.querySelectorAll('.frame');
-//var teamRed = document.querySelector('.red');
 var box = document.querySelector('.box');
 
-Array.from(teamBlue).forEach(element => element.addEventListener('click', changeTeam));
-//teamRed.addEventListener('click', changeTeam);
+Array.from(teamBlue).forEach(element => element.addEventListener('click', changeFrame));
 
-function changeTeam() {
+function changeFrame() {
   var rect = box.getBoundingClientRect();
-  var classes = this.classList;
   this.appendChild(box);
 
   TweenMax.set(box, { x: 0, y: 0 });
-  /*
-  if (classes.contains('red')) {
-    TweenMax.to(box, 1, { backgroundColor: 'red' });
-  } else if (classes.contains('blue')) {
-    TweenMax.to(box, 1, { backgroundColor: 'blue' });
-  }
-*/
+
   var newRect = box.getBoundingClientRect();
 
   TweenMax.from(box, 1, {
     x: rect.left - newRect.left,
     y: rect.top - newRect.top,
-    ease: Power3.easeOut
+    ease: Power1.easeInOut
   });
 }
 
