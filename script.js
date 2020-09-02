11ed9da96c8d75926f36da6ca824f86ef57a3811
Bojo <bozhidar_zahariev@hotmail.com>
2020-09-02 15:52:07 +0100

    confirm div

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const frames = document.querySelectorAll('.frame');
 const box = document.querySelector('.box');
 const dice = document.querySelector('#dice');
+const cardDiv = document.querySelector('#cardDiv');
 moved = true;
 
 const changeFrame = (step, move) => {
@@ -30,7 +31,7 @@ const changeFrame = (step, move) => {
 
   setTimeout(function() {
     framesControl(Number(trapValue), box.parentElement);
-
+    console.log(Array.from(frames)[Array.from(frames).indexOf(box.parentElement)].textContent);
     Array.from(frames)[Array.from(frames).indexOf(box.parentElement)].scrollIntoView({
       behavior: 'smooth'
     });
@@ -51,12 +52,22 @@ const rollDice = () => {
 };
 
 framesControl = (boxValue, frame) => {
-  console.log(boxValue);
   if (boxValue === 0) {
     moved = true;
   } else if (frame.classList.contains('trap')) {
-    changeFrame(boxValue, 'back');
+    trapFrame(boxValue, 'back');
+    //changeFrame(boxValue, 'back');
   } else if (frame.classList.contains('boost')) {
-    changeFrame(boxValue, 'forward');
+    trapFrame(boxValue, 'forward');
+    //changeFrame(boxValue, 'forward');
   }
+};
+
+const trapFrame = (boxValue, direction) => {
+  cardDiv.style.display = 'block';
+
+  document.querySelector('#confirm').addEventListener('click', () => {
+    changeFrame(boxValue, direction);
+    cardDiv.style.display = 'none';
+  });
 };
